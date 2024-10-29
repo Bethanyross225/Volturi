d420a684c6322f3f487be6bec7c61b6e68fdabe7
Jax <jbraith29@gmail.com>
2024-10-29 10:37:47 +0000

    Amended FizzBuzz tests

--- Fizzbuzz/src/FizzBuzz.java
+++ Fizzbuzz/src/FizzBuzz.java
@@ -3,7 +3,28 @@ import java.util.Scanner;
 
 public class FizzBuzz {
 
+    public playFizzBuzz (int user_input) {
+        String[] outcome = new String[user_input];
 
+        for(int i = 1; i <= user_input; i ++){
+
+            if ( i % 3 == 0 && i % 5 == 0){
+                outcome[i-1] = "FizzBuzz";
+//                System.out.println("FizzBuzz");
+
+            } else if (i % 3 == 0) {
+                outcome[i-1] = "Fizz";
+//                System.out.println("Fizz");
+
+            } else if(i % 5 == 0){
+                outcome[i-1] = "Buzz";
+//                System.out.println("Buzz");
+
+            }else{
+                outcome[i-1] = i.toString();
+//                System.out.println(i);
+            }
+    }
 
 
     public static void main(String[] args) {
@@ -11,23 +32,8 @@ public class FizzBuzz {
 
         //max value
         Scanner input = new Scanner(System.in);
-
         int max = input.nextInt();
 
-        for(int i = 1; i <= max; i ++){
-
-            if ( i % 3 == 0 && i % 5 == 0){
-                System.out.println("FizzBuzz");
-
-            } else if (i % 3 == 0) {
-                System.out.println("Fizz");
-
-            } else if(i % 5 == 0){
-                System.out.println("Buzz");
-
-            }else{
-                System.out.println(i);
-            }
 
 
         }

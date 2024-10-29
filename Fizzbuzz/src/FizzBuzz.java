9a01c3b057a4d3bfdd38c483d96d0e8ea618334e
Jax <jbraith29@gmail.com>
2024-10-29 10:08:58 +0000

    fixed FizzBuzz

--- Fizzbuzz/src/FizzBuzz.java
+++ Fizzbuzz/src/FizzBuzz.java
@@ -20,10 +20,10 @@ public class FizzBuzz {
                 System.out.println("FizzBuzz");
 
             } else if (i % 3 == 0) {
-                System.out.println("Buzz");
+                System.out.println("Fizz");
 
             } else if(i % 5 == 0){
-                System.out.println("Fizz");
+                System.out.println("Buzz");
 
             }else{
                 System.out.println(i);
